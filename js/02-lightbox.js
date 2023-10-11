ee2e7d7bb7d18b98f271489230c6887b05227565
viktormashyka <99292051+viktormashyka@users.noreply.github.com>
2023-10-11 18:08:41 +0100

    completed task #2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const galleryList = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    (el) =>
+      `<li class="gallery__item">
+  <a class="gallery__link" href=${el.original}>
+    <img class="gallery__image" src=${el.preview} alt=${el.description} title=${el.description}/>
+  </a>
+</li>`
+  )
+  .join("");
+
+galleryList.insertAdjacentHTML("beforeend", markup);
+
+galleryList.addEventListener("click", onOpenModalImg);
+
+function onOpenModalImg(e) {
+  e.preventDefault();
+
+  var lightbox = new SimpleLightbox(".gallery a", {
+    /* options */
+    captions: true,
+    captionPosition: "bottom",
+    captionDelay: 250,
+  });
+}
 
 console.log(galleryItems);
